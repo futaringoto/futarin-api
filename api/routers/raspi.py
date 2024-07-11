22b48704e855c31645822c51e13a00ad4f605d1f
shun-harutaro <shun_harutaro@protonmail.com>
2024-07-12 00:38:58 +0900

    services/voicevox.pyへ分離

--- api/routers/raspi.py
+++ api/routers/raspi.py
@@ -1,8 +1,9 @@
-import json
+from typing import Any, Dict
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse, JSONResponse
+from services.voicevox import audio_query, synthesis
 from services.whisper import speech2text
-import httpx
+from httpx import RequestError, HTTPStatusError
 import tempfile
 import os
 
@@ -16,40 +17,17 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
     tags=["voicevox クエリ作成"],
     summary="音声合成用のクエリを作成する"
 )
-async def audio(
-    text: str,
-    speaker: int = 1,
-):
+async def audio(text: str, speaker: int = 1):
     try:
-        url = os.getenv("VOICEVOX_URL")
-        params = {"text": text, "speaker": speaker}
-
-        async with httpx.AsyncClient() as client:
-            res1 = await client.post(
-                url + "/audio_query",
-                params=params
-            )
-            res1.raise_for_status()
-            query = res1.json()
-
-            res2 = await client.post(
-                url + "/synthesis",
-                params={"speaker": speaker},
-                data=json.dumps(query),
-                headers={"Content-Type": 'application/json'}
-            )
-            res2.raise_for_status()
-
-            # 一時ファイル作成
-            temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
-            with open(temp_file.name, 'wb') as f:
-                f.write(res2.content)
-
-    except httpx.RequestError as e:
+        query: Dict[str, Any] = await audio_query(text, speaker)
+        content: bytes = await synthesis(query, speaker)
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+        with open(temp_file.name, 'wb') as f:
+            f.write(content)
+    except RequestError as e:
         raise HTTPException(status_code=500, detail=f"Error fetching data: {str(e)}")
-    except httpx.HTTPStatusError as e:
+    except HTTPStatusError as e:
         raise HTTPException(status_code=e.response.status_code, detail=f"Error fetching data: {str(e)}")
-
     return FileResponse(
         temp_file.name,
         media_type="audio/wav",
