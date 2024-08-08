75a3f93eaad3f9c10db66682beeb4de8299faa09
shun-harutaro <shun_harutaro@protonmail.com>
2024-08-09 07:06:35 +0900

    pytestの導入[#26]

---
+++ api/tests/test_main.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+@pytest.mark.asyncio
+async def test_transcribe_and_respond(client: TestClient):
+    audio_file_path = "tests/audio1.wav"
+    with open(audio_file_path, "rb") as audio_file:
+        files = {'file': (audio_file_path, audio_file, 'multipart/form-data')}
+
+        response = await client.post("/raspi", files=files)
+
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "audio/wav"
+    assert response.content is not None
