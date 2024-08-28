4591243fa10343b4bf97a953b5ed072627ac65d6
harudra <harudra111@gmail.com>
2024-08-28 21:42:13 +0900

    wip

--- _docker/db/sql/create_table.sql
+++ _docker/db/sql/create_table.sql
@@ -12,13 +12,13 @@ CREATE TABLE users (
     PRIMARY KEY (uuid),
     FOREIGN KEY (raspi_id) REFERENCES raspis (raspi_id),
     FOREIGN KEY (group_id) REFERENCES groups (group_id)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 CREATE TABLE raspis (
     raspi_id CHAR(36),
     mac_address CHAR(17),
     PRIMARY KEY (raspi_id)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 CREATE TABLE groups (
     group_id CHAR(36),
@@ -27,7 +27,7 @@ CREATE TABLE groups (
     PRIMARY KEY (group_id),
     FOREIGN KEY (uuid_1) REFERENCES users (uuid),
     FOREIGN KEY (uuid_2) REFERENCES users (uuid)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 CREATE TABLE texts (
     text_id CHAR(36),
@@ -37,7 +37,7 @@ CREATE TABLE texts (
     created_at DATE,
     PRIMARY KEY (text_id),
     FOREIGN KEY (uuid) REFERENCES users (uuid)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 CREATE TABLE messages (
     message_id CHAR(36),
@@ -45,4 +45,4 @@ CREATE TABLE messages (
     send_message VARCHAR(1000),
     PRIMARY KEY (message_id),
     FOREIGN KEY (uuid) REFERENCES users (uuid)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
