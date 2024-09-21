50185e1de381c501a79fbcff29725fe1041faafa
harudra <harudra111@gmail.com>
2024-09-21 22:41:51 +0900

    Revert "insert db"
    This reverts commit fd0d6668071f8af6298bdfcc8e05aa5a027a013e.

---
+++ api/v1/services/db.py
@@ -0,0 +1,12 @@
+import mysql.connector
+from v1.utils.config import get_mysql_root_pass, get_mysql_name
+
+conn = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    user="root",
+    password=get_mysql_root_pass(),
+    database=get_mysql_name(),
+)
+
+conn.ping(reconnect=True)
