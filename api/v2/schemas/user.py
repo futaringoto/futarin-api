e6446ec705b562b7e7f7c3f446dd291ba9e079a0
shun-harutaro <shun_harutaro@protonmail.com>
2024-09-22 23:10:20 +0900

    userのリクエストスキーマ定義

--- api/v2/schemas/user.py
+++ api/v2/schemas/user.py
@@ -7,16 +7,27 @@ def generate_default_username() -> str:
     return f"user_{datetime.now().strftime('%Y%m%d%H%M%S')}"
 
 
-class User(BaseModel):
-    id: Optional[int] = Field(
-        None,
-        description="Auto-incremented ID, set by the database",
-    )
+class UserBase(BaseModel):
     couple_id: Optional[int] = Field(
         None,
         description="couple_id is optional",
     )
-    username: str = Field(default_factory=generate_default_username)
+    username: str = Field(..., default_factory=generate_default_username)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserUpdate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    id: Optional[int] = Field(
+        None,
+        description="Auto-incremented ID, set by the database",
+    )
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(),
     )
