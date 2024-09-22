66206eeb25bfff734499862653d2e695aa6c541f
shun-harutaro <shun_harutaro@protonmail.com>
2024-09-22 21:50:11 +0900

    userのスキーマ定義

---
+++ api/v2/schemas/user.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+def generate_default_username() -> str:
+    return f"user_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+
+
+class User(BaseModel):
+    id: Optional[int] = Field(
+        None,
+        description="Auto-incremented ID, set by the database",
+    )
+    couple_id: Optional[int] = Field(
+        None,
+        description="couple_id is optional",
+    )
+    username: str = Field(default_factory=generate_default_username)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+    )
