546abad184d0e5180dd5c82c333849e7b1ff3b56
shun-harutaro <shun_harutaro@protonmail.com>
2024-09-22 20:18:32 +0900

    /couplesの定義

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ from v1.routers import raspi as v1_raspi
 from v1.routers import sandbox as v1_sandbox
 from v1.utils.config import check_env_variables as v1_check_env_variables
 from v2.routers import user as v2_user
+from v2.routers import couple as v2_couple
 
 
 @asynccontextmanager
@@ -54,6 +55,7 @@ app.include_router(v0_raspi.router)
 app.include_router(v1_raspi.router, prefix="/v1/raspi")
 app.include_router(v1_sandbox.router, prefix="/v1/sandbox")
 app.include_router(v2_user.router, prefix="/v2/users")
+app.include_router(v2_couple.router, prefix="/v2/couples")
 
 
 @app.get("/")
