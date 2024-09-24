bbe199adeab6bb0b1826776785176c8474710572
NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
2024-09-25 08:14:29 +0900

    Assistants を、既存のIDで動作するよう変更 (#145)

--- api/v1/services/gpt.py
+++ api/v1/services/gpt.py
@@ -1,61 +1,31 @@
 from openai import OpenAI
-from v1.utils.config import get_openai_api_key
-import os
+
+from v1.utils.config import (
+    get_openai_api_key,
+    get_openai_assistant_id,
+    get_openai_thread_id,
+)
 
 OpenAI.api_key = get_openai_api_key()
+ASSISTANT_ID = get_openai_assistant_id()
+THREAD_ID = get_openai_thread_id()
 client = OpenAI()
 
 
 def generate_text(prompt: str) -> str:
-    assistant = client.beta.assistants.create(
-        name="futarin",
-        instructions="あなたは、プロのカウンセラーです。\
-            与えられたファイルをもとに相談者の気持ちに寄り添い、解決策を提示しながら、簡潔でフレンドリーなひとまとまりのメッセージを生成してください。\
-            日本語で50文字程度でお願いします。\
-        ",
-        model="gpt-4o-mini",
-        tools=[{"type": "file_search"}],
-    )
-
-    vs_name = "professional counselor"
-    vector_store = client.beta.vector_stores.create(name=vs_name)
-
-    # このスクリプトが存在するディレクトリのパスを取得
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # cheering.txt への相対パスを作成
-    file_path = os.path.join(current_dir, "../utils/cheering.txt")
-
-    # ファイルを開く
-    # with open(file_path, 'r') as file:
-    #     file_streams = file.read()
-
-    file_streams = [open(file_path, "rb")]
-
-    client.beta.vector_stores.file_batches.upload_and_poll(
-        vector_store_id=vector_store.id, files=file_streams
-    )
-
-    assistant = client.beta.assistants.update(
-        assistant_id=assistant.id,
-        tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
-    )
-
-    thread = client.beta.threads.create()
-
     client.beta.threads.messages.create(
-        thread_id=thread.id,
+        thread_id=THREAD_ID,
         role="user",
         content=prompt,
     )
 
     run = client.beta.threads.runs.create_and_poll(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
+        thread_id=THREAD_ID,
+        assistant_id=ASSISTANT_ID,
     )
 
     messages = list(
-        client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id)
+        client.beta.threads.messages.list(thread_id=THREAD_ID, run_id=run.id)
     )
     message_content = messages[0].content[0].text
     annotations = message_content.annotations
