750e4c2b49417f2ca875a06dc983df84a9f7b76f
harudra <harudra111@gmail.com>
2024-09-27 13:03:55 +0900

    post

--- api/v2/models/user.py
+++ api/v2/models/user.py
@@ -9,11 +9,11 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    couple_id = Column(Integer, ForeignKey("couples.id"))
+    couple_id = Column(Integer, ForeignKey("couples.id"), nullable=True)
     name = Column(String(20))
     created_at = Column(TIMESTAMP, server_default=func.now())
     updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
-    couple = relationship("Couple", back_populates="couples")
+    couple = relationship("Couple", back_populates="users")
     texts = relationship("Text", back_populates="user")
     messages = relationship("Message", back_populates="user")
