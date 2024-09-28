589d008840391396ca9c0545c3f8d5078f7661e0
NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
2024-09-29 06:07:56 +0900

    SQLの制約の見直し (#134)
    * update sql
    
    * Textテーブルの除去
    
    * modify sql
    
    * エラー修正
    
    ---------
    
    Co-authored-by: harudra <harudra111@gmail.com>

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 from db import Base
-from v2.models import Couple, Message, Text, User  # noqa: F401
+from v2.models import Couple, Message, User  # noqa: F401
 from v2.utils.config import get_db_url
 
 DB_URL = get_db_url()
