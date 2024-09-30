eb4a428b69aa370ed7640e76d054754846c78067
Haruya <137756065+harudra@users.noreply.github.com>
2024-09-30 13:15:27 +0900

    Feat/#138 (#156)
    * post
    
    * post
    
    * formatted
    
    * reviewed
    
    * get
    
    * completed api
    
    * completed api for couples table
    
    * applied formatter
    
    * update by reviews
    
    ---------
    
    Co-authored-by: shun-harutaro <shun_harutaro@protonmail.com>

--- api/v2/cruds/user.py
+++ api/v2/cruds/user.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import v2.models.user as user_model
@@ -13,3 +15,40 @@ async def create_user(
     await db.commit()
     await db.refresh(user)
     return user
+
+
+async def get_users(db: AsyncSession):
+    result: Result = await db.execute(
+        select(
+            user_model.User.id,
+            user_model.User.couple_id,
+            user_model.User.name,
+            user_model.User.created_at,
+            user_model.User.updated_at,
+        )
+    )
+    return result.all()
+
+
+async def get_user(db: AsyncSession, user_id: int):
+    result: Result = await db.execute(
+        select(user_model.User).filter(user_model.User.id == user_id)
+    )
+    user = result.first()
+    return user[0] if user is not None else None
+
+
+async def update_user(
+    db: AsyncSession, user_update: user_schema.UserUpdate, original: user_model.User
+) -> user_model.User:
+    original.couple_id = user_update.couple_id
+    original.name = user_update.name
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
+
+
+async def delete_user(db: AsyncSession, original: user_model.User) -> None:
+    await db.delete(original)
+    await db.commit()
