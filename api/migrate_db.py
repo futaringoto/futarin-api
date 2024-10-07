55eb0a0ef825919b48d825454f694231e3f7ab34
shun-harutaro <shun_harutaro@protonmail.com>
2024-10-08 07:55:46 +0900

    マージ前の修正

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -1,10 +1,8 @@
-from azure.storage.blob import BlobServiceClient
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 
 from db import Base
 from v2.models import Couple, Message, User  # noqa: F401
-from v2.utils.config import get_azure_sas_token, get_azure_storage_account
 
 DB_URL = URL.create(
     drivername="mysql+pymysql",
@@ -16,12 +14,6 @@ DB_URL = URL.create(
 )
 
 engine = create_engine(DB_URL, echo=True)
-
-# azureの認証
-azure_storage_account = get_azure_storage_account()
-account_url = f"https://{azure_storage_account}.blob.core.windows.net"
-sas_token = get_azure_sas_token()
-blob_service_client = BlobServiceClient(account_url, credential=sas_token)
 
 
 def reset_database():
