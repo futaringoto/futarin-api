8e380e925887054ec7dd732c799794e8b06d0f00
shun-harutaro <shun_harutaro@protonmail.com>
2024-10-09 14:43:05 +0900

    lint

--- api/v2/services/pubsub.py
+++ api/v2/services/pubsub.py
@@ -1,4 +1,5 @@
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
+
 from v2.utils.config import get_pubsub_connection_string
 
 CONNECTION_STRING = get_pubsub_connection_string()
@@ -7,14 +8,12 @@ CONNECTION_STRING = get_pubsub_connection_string()
 def get_service() -> WebPubSubServiceClient:
     service = WebPubSubServiceClient.from_connection_string(
         CONNECTION_STRING,
-        hub = f"futarin_raspi",
+        hub="futarin_raspi",
     )
     return service
 
 
 def push_id(id: int):
     service = get_service()
-    res = service.send_to_all(
-        {"user_id": str(id)}
-    )
+    res = service.send_to_all({"user_id": str(id)})
     return res
