286fc522fbb3eaedbd56dfcc2b4bb6bb8329c7f6
NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
2024-10-10 13:30:59 +0900

    raspi テーブル作成 (#189)
    * Update ER.md
    
    * Raspiテーブル・モデル作成
    
    * Raspiスキーマ定義
    
    * ルーティング定義
    
    * 動いた

--- api/v2/models/__init__.py
+++ api/v2/models/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from .couple import Couple
 from .message import Message
+from .raspi import Raspi
 from .user import User
--- api/v2/schemas/raspi.py
+++ api/v2/schemas/raspi.py
@@ -1,10 +1,36 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict, Field
+
+
+def generate_default_raspiname() -> str:
+    return f"raspi_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+
+
+class RaspiMessageResponse(BaseModel):
+    message: str = Field(None, json_schema_extra={"example": "successed!"})
 
 
 class RaspiBase(BaseModel):
     model_config = ConfigDict(extra="forbid")
+    name: str = Field(default_factory=generate_default_raspiname)
 
 
-class RaspiMessageResponse(RaspiBase):
-    id: int
-    message: str = Field(None, json_schema_extra={"example": "successed!"})
+class RaspiCreate(RaspiBase):
+    pass
+
+
+class RaspiUpdate(RaspiBase):
+    pass
+
+
+class RaspiResponse(RaspiBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: Optional[int] = Field(None, description="Auto-increment")
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(),
+    )
