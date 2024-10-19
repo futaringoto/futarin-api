c40d0d4deeec7ba166d39b9bda676e2a57621723
Haruya <137756065+harudra@users.noreply.github.com>
2024-10-20 01:16:40 +0900

    Feat/#178 (#224)
    * sendmessageを改善
    
    * クエリパラメータでmode選択
    
    * deploy.ymlこうしん (#219)
    
    * Revert "userのエラーハンドリング (#217)" (#220)
    
    This reverts commit bc410e8d07665f55e5e622f953bd637afd0d92cc.
    
    * Revert "deploy.ymlこうしん (#219)" (#221)
    
    This reverts commit 80ca27a4c1cb673e3840f486b8d6a234fe6f54a8.
    
    * Revert "Revert "userのエラーハンドリング (#217)" (#220)"
    
    This reverts commit 9f60b890a4ec78d29682aa827c80ee4f0c315bcb.
    
    * ChatGPT transfer
    
    * ChatGPT transfer
    
    * 環境変数の追加
    
    ---------
    
    Co-authored-by: shun-harutaro <shun_harutaro@protonmail.com>
    Co-authored-by: NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>

--- api/v2/services/gpt.py
+++ api/v2/services/gpt.py
@@ -1,11 +1,16 @@
 from openai import AsyncOpenAI
 
-from config import get_openai_api_key, get_openai_assistant_id
+from config import (
+    get_openai_api_key,
+    get_openai_assistant_id,
+    get_openai_assistant_id_paraphrase,
+)
 
 AsyncOpenAI.api_key = get_openai_api_key()
 client = AsyncOpenAI()
 
 ASSISTANT_ID = get_openai_assistant_id()
+ASSISTANT_ID_PARAPHRASE = get_openai_assistant_id_paraphrase()
 
 
 async def create_new_thread_id() -> str:
@@ -20,7 +25,7 @@ async def delete_thread_id(thread_id: str):
 async def generate_text(mode: int, thread_id: int, prompt: str) -> str:
     mode_dispatch = {
         0: generate_text_normal,
-        1: generate_text_normal,
+        1: generate_text_change,
     }
 
     try:
@@ -41,6 +46,37 @@ async def generate_text_normal(thread_id: int, prompt: str) -> str:
     run = await client.beta.threads.runs.create_and_poll(
         thread_id=thread_id,
         assistant_id=ASSISTANT_ID,
+    )
+
+    messages = await client.beta.threads.messages.list(
+        thread_id=thread_id, run_id=run.id
+    )
+    message_content = messages.data[0].content[0].text
+    annotations = message_content.annotations
+    citations = []
+
+    for index, annotation in enumerate(annotations):
+        message_content.value = message_content.value.replace(
+            annotation.text, f"[{index}]"
+        )
+
+        if file_citation := getattr(annotation, "file_citation", None):
+            cited_file = client.files.retrieve(file_citation.file_id)
+            citations.append(f"[{index}] {cited_file.filename}")
+
+    return message_content.value
+
+
+async def generate_text_change(thread_id: int, prompt: str) -> str:
+    await client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=prompt,
+    )
+
+    run = await client.beta.threads.runs.create_and_poll(
+        thread_id=thread_id,
+        assistant_id=ASSISTANT_ID_PARAPHRASE,
     )
 
     messages = await client.beta.threads.messages.list(
